4c74307ad4ce18f7a7b220fded5f9be8fd307c47
Davide Salerno <dsalerno@redhat.com>
2022-12-23 13:44:04 +0100

    [KOGITO-8321] Add the Buildah support into container-builder module
    Signed-off-by: Davide Salerno <dsalerno@redhat.com>

--- api/build_type_support.go
+++ api/build_type_support.go
@@ -1,6 +1,9 @@
 package api
 
-import "path"
+import (
+	"github.com/kiegroup/container-builder/util/defaults"
+	"path"
+)
 
 func NewKanikoBuild(platformBuild PlatformBuild, publishImage string, buildName string) *Build {
 	result := &Build{
@@ -15,6 +18,29 @@ func NewKanikoBuild(platformBuild PlatformBuild, publishImage string, buildName 
 						Registry:   platformBuild.Spec.Registry,
 					},
 					Cache: KanikoTaskCache{},
+				}},
+			},
+			Strategy: BuildStrategyPod,
+			Timeout:  *platformBuild.Spec.Timeout,
+		},
+	}
+	result.Name = buildName
+	result.Namespace = platformBuild.Namespace
+	return result
+}
+
+func NewBuildahBuild(platformBuild PlatformBuild, publishImage string, buildName string) *Build {
+	result := &Build{
+		Spec: BuildSpec{
+			Tasks: []Task{
+				{Buildah: &BuildahTask{
+					BaseTask: BaseTask{Name: "BuildahTask"},
+					PublishTask: PublishTask{
+						BaseImage: platformBuild.Spec.BaseImage,
+						Image:     publishImage,
+						Registry:  platformBuild.Spec.Registry,
+					},
+					ExecutorImage: defaults.BuildahDefaultImageName,
 				}},
 			},
 			Strategy: BuildStrategyPod,
---
+++ util/defaults/buildah.go
@@ -0,0 +1,7 @@
+package defaults
+
+const (
+	// BuildahVersion --
+	BuildahVersion          = "1.23.3"
+	BuildahDefaultImageName = "quay.io/buildah/stable:v" + BuildahVersion
+)
